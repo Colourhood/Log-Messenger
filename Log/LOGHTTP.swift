3a39c7204f4730494d662531256c2fd7333e4343
Andrei <andreicrimson@gmail.com>
2017-09-08 23:00:04 -0700

    refactored LOGHTTP model, also GET req request has to use the URLEncoding in order to work

--- Log/LOGHTTP.swift
+++ Log/LOGHTTP.swift
@@ -15,36 +15,11 @@ let httpHeaders: HTTPHeaders = [ "Accept": "application/json" ];
 
 struct LOGHTTP {
     
-    func get(url: String, parameters: Parameters) -> Void {
-        Alamofire.request(apiURL+url, method: .get, parameters: parameters, encoding: JSONEncoding.default, headers: httpHeaders).responseJSON { (response) in
-            print(response.request as Any);
-            print(response.response as Any);
-            print(response.data as Any);
-            print(response.result);
-            
-            if let JSON = response.result.value {
-                print("JSON: \n \(JSON)")
-            }
-        }
+    func get(url: String, parameters: Parameters) -> Alamofire.DataRequest {
+        return Alamofire.request(apiURL+url, method: .get, parameters: parameters, encoding: URLEncoding.default);
     }
     
     func post(url: String, parameters: Parameters) -> Alamofire.DataRequest {
-        let request = Alamofire.request(apiURL+url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: httpHeaders);
-        //{ (response) in
-//            print(response.request!);
-//            print(response.response!);
-//            print(response.data!);
-//            print(response.result);
-//            
-//            switch(response.result) {
-//                case .success(let data):
-//                    print("It was a success \(data)");
-//                    break;
-//                case .failure(let error):
-//                    print("Error \(error)");
-//                    break;
-//            }
-//        }
-        return request;
+        return Alamofire.request(apiURL+url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: httpHeaders);
     }
 }
