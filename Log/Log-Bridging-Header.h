3470e518560fe614c524481ba930b1e3c73abd10
Andrei <andreicrimson@gmail.com>
2017-09-14 09:21:29 -0700

    Integrated the Image Picker and Image Cropper into the project - sign up view

---
+++ Log/Log-Bridging-Header.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+// Import 'RSKImageCropper' Dependencies
+#import <CoreGraphics/CoreGraphics.h>
+
+// Import RSKImageCropper Framework
+#import <RSKImageCropper/RSKImageCropper.h>
--- Log/SignInViewController.swift
+++ Log/SignInViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import ImagePicker
 
 class InitialViewController: UIViewController {
     @IBOutlet weak var signUpButton: UIButton!;
@@ -36,19 +37,32 @@ class InitialViewController: UIViewController {
 
 class SignInViewController: UIViewController {
     
+    @IBOutlet weak var imageButton: UIButton!
     @IBOutlet weak var emailTextField: UITextField!;
     @IBOutlet weak var passwordTextField: UITextField!;
     @IBOutlet weak var signInButton: UIButton!;
     
     var loginOrSignupTypeText: String?;
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let _ = imageButton {
+            imageButton.layer.masksToBounds = true;
+            imageButton.translatesAutoresizingMaskIntoConstraints = false;
+            imageButton.layer.cornerRadius = (imageButton.frame.height / 2);
+        }
     }
     
     @IBAction func logUserIn() {
         checkTextField();
+    }
+    
+    @IBAction func chooseButton() {
+        let imagePickerController = ImagePickerController();
+        imagePickerController.delegate = self;
+        imagePickerController.imageLimit = 1;
+        present(imagePickerController, animated: true, completion: nil);
     }
     
     func setUserInCoreData(username: String) {
@@ -127,9 +141,7 @@ extension SignInViewController: UITextFieldDelegate {
     }
     
     /* UITextField Delegate Methods*/
-    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
         switch(textField.tag) {
             case TextFieldTags.email.rawValue:
                 let nextResponder: UIResponder!;
@@ -147,3 +159,41 @@ extension SignInViewController: UITextFieldDelegate {
     }
     
 }
+
+extension SignInViewController: ImagePickerDelegate {
+    
+    func wrapperDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {}
+    
+    func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        let image: UIImage? = images[0];
+        self.dismiss(animated: true) { 
+            let imageCropperViewController = RSKImageCropViewController.init(image: image!, cropMode: RSKImageCropMode.circle);
+            imageCropperViewController.delegate = self;
+            
+            self.present(imageCropperViewController, animated: true, completion: nil);
+        }
+    }
+    
+    func cancelButtonDidPress(_ imagePicker: ImagePickerController) {}
+}
+
+extension SignInViewController: RSKImageCropViewControllerDelegate {
+    
+    func imageCropViewControllerDidCancelCrop(_ controller: RSKImageCropViewController) {
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+    func imageCropViewController(_ controller: RSKImageCropViewController, willCropImage originalImage: UIImage) {}
+    
+    func imageCropViewController(_ controller: RSKImageCropViewController, didCropImage croppedImage: UIImage, usingCropRect cropRect: CGRect) {
+        self.imageButton.setImage(croppedImage, for: .normal);
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+    func imageCropViewController(_ controller: RSKImageCropViewController, didCropImage croppedImage: UIImage, usingCropRect cropRect: CGRect, rotationAngle: CGFloat) {
+        self.imageButton.setImage(croppedImage, for: .normal);
+        self.dismiss(animated: true, completion: nil);
+    }
+}
+
+
