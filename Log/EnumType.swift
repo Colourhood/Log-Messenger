df2b1b55b0a8245080b67ea3fd2e94be98c9d2df
Andrei <andreicrimson@gmail.com>
2017-09-21 01:22:16 -0700

    Enumeration struct for different type of data

---
+++ Log/EnumType.swift
@@ -0,0 +1,40 @@
+//
+//  FileTypes.swift
+//  Log
+//
+//  Created by Andrei Villasana on 9/20/17.
+//  Copyright © 2017 Andrei Villasana. All rights reserved.
+//
+
+import Foundation
+
+struct EnumType {
+    
+    static let allDirectories = [dir.Images];
+    
+    enum ext: String {
+        case JSON = "json"
+        case Text = "txt"
+        case PDF = "pdf"
+    }
+    
+    enum img: String {
+        case PNG = "png"
+        case JPEG = "jpeg"
+        case JPG = "jpg"
+        case TIFF = "tiff"
+    }
+    
+    
+    enum dir: String {
+        case Images = "Images"
+    }
+    
+    enum imgf: String {
+        case profilePicture = "profilePicture"
+    }
+    
+    
+
+    
+}
