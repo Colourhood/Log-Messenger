ff67b39e1e23f7866ea918728ae744d658aac750
Andrei <andreicrimson@gmail.com>
2017-10-15 00:53:07 -0700

    integrating socketio to the project and basic functions for it.

---
+++ Log/SocketIOManager.swift
@@ -0,0 +1,43 @@
+//
+//  File.swift
+//  Log
+//
+//  Created by Andrei Villasana on 10/12/17.
+//  Copyright © 2017 Andrei Villasana. All rights reserved.
+//
+
+import Foundation
+import SocketIO
+
+private let socketURL: String = "http://127.0.0.1:7555";
+
+class SocketIOManager: NSObject {
+    static let sharedInstance = SocketIOManager(); //- singleton possibility; manage multiple sockets in the app?
+    var socket: SocketIOClient = SocketIOClient(socketURL: URL(string: socketURL)!);
+
+    private override init() {
+        super.init();
+    }
+
+    func establishConnection() {
+        socket.connect();
+    }
+
+    func closeConnection() {
+        socket.disconnect();
+    }
+
+    func addHandler(event: String) {
+        socket.on(event) { (data, _) in
+            print("Received event \(data[0])");
+        }
+    }
+
+    func removeHandler(event: String) {
+        socket.off(event);
+    }
+
+    func emitToEvent(event: String, message: String) {
+        socket.emit(event, message);
+    }
+}
