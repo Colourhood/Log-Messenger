a634244d228c247315a5162bb13277a239b831e4
Andrei <andreicrimson@gmail.com>
2017-11-07 00:24:37 -0800

    clean up sample log test

--- LogTests/LogTests.swift
+++ LogTests/LogTests.swift
@@ -11,41 +11,23 @@ import XCTest
 
 class LogTests: XCTestCase {
 
-    var userEmail: String?
-    var password: String?
-
     override func setUp() {
         super.setUp()
-        self.userEmail = "andreicrimson@gmail.com"
-        self.password = "password"
     }
 
     override func tearDown() {
         super.tearDown()
-        self.userEmail = nil
-        self.password = nil
     }
 
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let promise = expectation(description: "Getting data from server")
-        MessageController.getMessagesForFriend(friendEmail: "andreicrimson@gmail.com", completionHandler: { data in
-            print("Received data: \(data)")
-            promise.fulfill()
-        })
-
-        waitForExpectations(timeout: 5.0) { (error) in
-            print("There was an error \(error!)")
-        }
-
     }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
-            testExample()
         }
     }
 
