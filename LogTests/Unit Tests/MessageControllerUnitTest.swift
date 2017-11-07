188d11c961d523ad412fb4bd7ab4d5f0811938f3
Andrei <andreicrimson@gmail.com>
2017-11-07 00:25:19 -0800

    create a functional mock core data representation of UserCoreData for InMemoryStoreType.

--- LogTests/Unit Tests/MessageControllerUnitTest.swift
+++ LogTests/Unit Tests/MessageControllerUnitTest.swift
@@ -7,12 +7,43 @@
 //
 
 import XCTest
+@testable import Log
+import CoreData
 
 class MessageControllerUnitTest: XCTestCase {
+
+    var mockManageObjectContext: NSManagedObjectContext?
+    var userEntity: UserCoreData?
+
+    func setupInMemoryManagedObjectContext() -> NSManagedObjectContext {
+        let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle.main])
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel!)
+
+        do {
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSInMemoryStoreType, configurationName: nil, at: nil, options: nil)
+        } catch {
+            print("Adding persistent store failed")
+        }
+        let managedObjectContext = NSManagedObjectContext.init(concurrencyType: NSManagedObjectContextConcurrencyType.mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+
+        return managedObjectContext
+    }
 
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        mockManageObjectContext = setupInMemoryManagedObjectContext()
+        userEntity = NSEntityDescription.insertNewObject(forEntityName: "User", into: mockManageObjectContext!) as? UserCoreData
+
+        userEntity?.email = "andreicrimson@gmail.com"
+        userEntity?.firstName = "Andrei"
+        userEntity?.lastName = "Villasana"
+    }
+
+    func testUserCoreData() {
+        XCTAssertEqual(userEntity?.email, "andreicrimson@gmail.com")
     }
 
     override func tearDown() {
