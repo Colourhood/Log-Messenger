83fe6748ec47b79da7252b72a4b221e89b8aad60
Andrei <andreicrimson@gmail.com>
2017-12-01 15:29:03 -0800

    refactor and clean unused method calls

--- Log/SegueFromRight.swift
+++ Log/SegueFromRight.swift
@@ -23,16 +23,11 @@ class SegueFromRight: UIStoryboardSegue {
 
         containerView?.addSubview(destinationView.view)
 
-        UIView.animate(withDuration: 0.3,
-                       delay: 0,
-                       options: .curveEaseInOut,
-                       animations: {
-                        destinationView.view.transform = CGAffineTransform(translationX: 0, y: 0)
-                       },
-                       completion: { _ in
-                        sourceView.present(destinationView, animated: false, completion: nil)
-                       }
-        )
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut, animations: {
+            destinationView.view.transform = CGAffineTransform(translationX: 0, y: 0)
+        }, completion: { _ in
+            sourceView.present(destinationView, animated: false, completion: nil)
+        })
     }
 
 }
@@ -49,16 +44,11 @@ class UnwindSegueFromRight: UIStoryboardSegue {
 
         sourceView.view.superview?.insertSubview(destinationView.view, at: 0)
 
-        UIView.animate(withDuration: 0.3,
-                       delay: 0,
-                       options: .curveEaseIn,
-                       animations: {
-                        sourceView.view.transform = CGAffineTransform(translationX: sourceView.view.frame.width, y: 0)
-                       },
-                       completion: { _ in
-                        sourceView.dismiss(animated: false, completion: nil)
-                       }
-        )
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn, animations: {
+            sourceView.view.transform = CGAffineTransform(translationX: sourceView.view.frame.width, y: 0)
+        }, completion: { _ in
+            sourceView.dismiss(animated: false, completion: nil)
+        })
     }
 
 }
